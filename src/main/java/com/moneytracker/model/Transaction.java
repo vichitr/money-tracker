ac68292b4c852de57fe07fd2c17a537dbaa695ea
sanjay <sanjay.sofficial03@gmail.com>
2025-10-03 16:42:27 +0530

    adding fields (#3)
    * adding fields
    
    * addding setter and getter
    
    * removed unwanted changes
    
    * added field names
    
    * resolving errors

--- src/main/java/com/moneytracker/model/Transaction.java
+++ src/main/java/com/moneytracker/model/Transaction.java
@@ -14,24 +14,26 @@ public class Transaction {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
     
     @NotBlank(message = "Description is required")
-    @Column(nullable = false)
+    @Column(nullable = false, name = "description")
     private String description;
     
     @NotNull(message = "Amount is required")
     @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
     @DecimalMax(value = "10000000", message = "Amount must be lesser than 1 Cr")
-    @Column(nullable = false, precision = 10, scale = 2)
+    @Column(nullable = false, precision = 10, scale = 2, name = "amount")
     private BigDecimal amount;
     
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, name = "transaction_type")
     private TransactionType type;
     
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false , name = "category")
     private Category category;
     
     @Column(name = "created_at")
@@ -39,4 +41,85 @@ public class Transaction {
     
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    public Transaction() {}
+
+
+    public Transaction(Long id, String description, BigDecimal amount,
+                       TransactionType type, Category category,
+                       LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.type = type;
+        this.category = category;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Transaction(String description, BigDecimal amount, TransactionType type, Category category) {
+        this.description = description;
+        this.amount = amount;
+        this.type = type;
+        this.category = category;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
