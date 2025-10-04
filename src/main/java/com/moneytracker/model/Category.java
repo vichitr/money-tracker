4f6899a31a0aaceaf0252ce1f8a3fa2418b1cf60
sanjay <sanjay.sofficial03@gmail.com>
2025-10-04 22:11:29 +0530

    added new categories (#7)
    * added categories
    
    * Add new categories

--- src/main/java/com/moneytracker/model/Category.java
+++ src/main/java/com/moneytracker/model/Category.java
@@ -4,6 +4,8 @@ public enum Category {
     // Income Categories
     SALARY("Salary"),
     FREELANCE("Freelance"),
+    RENTAL_INCOME("Rental Income"),
+    TAX_REFUND("Tax Refund"),
     INVESTMENT("Investment"),
     BUSINESS("Business"),
     OTHER_INCOME("Other Income"),
@@ -14,8 +16,14 @@ public enum Category {
     ENTERTAINMENT("Entertainment"),
     SHOPPING("Shopping"),
     BILLS("Bills & Utilities"),
-    HEALTHCARE("Healthcare"),
     EDUCATION("Education"),
+    GROCERY("Grocery"),
+    GIFT("Gifs"),
+    HOUSEHOLD("Household"),
+    INSURANCE("Insurance"),
+    ELECTRONICS("Electronics"),
+    HEALTHCARE("Healthcare"),
+
     TRAVEL("Travel"),
     RENT("Rent & Housing"),
     OTHER_EXPENSE("Other Expense");
