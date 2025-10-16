e7ec46ed02a2868b24496d07fc13a5566b0a61ca
sanjay kumar <sanjay.sofficial03@gmail.com>
2025-10-16 17:42:30 +0530

    Got Cleanup

--- src/main/java/com/moneytracker/model/TransactionSummary.java
+++ src/main/java/com/moneytracker/model/TransactionSummary.java
@@ -3,12 +3,14 @@ package com.moneytracker.model;
 import java.math.BigDecimal;
 
 public class TransactionSummary {
+
   private BigDecimal totalIncome;
   private BigDecimal totalExpense;
   private BigDecimal balance;
   private int transactionCount;
 
-  public TransactionSummary(BigDecimal totalIncome, BigDecimal totalExpense, BigDecimal balance, int transactionCount) {
+  public TransactionSummary(BigDecimal totalIncome, BigDecimal totalExpense, BigDecimal balance,
+      int transactionCount) {
     this.totalIncome = totalIncome;
     this.totalExpense = totalExpense;
     this.balance = balance;
@@ -16,8 +18,19 @@ public class TransactionSummary {
   }
 
   // Getters
-  public BigDecimal getTotalIncome() { return totalIncome; }
-  public BigDecimal getTotalExpense() { return totalExpense; }
-  public BigDecimal getBalance() { return balance; }
-  public int getTransactionCount() { return transactionCount; }
+  public BigDecimal getTotalIncome() {
+    return totalIncome;
+  }
+
+  public BigDecimal getTotalExpense() {
+    return totalExpense;
+  }
+
+  public BigDecimal getBalance() {
+    return balance;
+  }
+
+  public int getTransactionCount() {
+    return transactionCount;
+  }
 }
